966f66b16ce7bf59ea18ec0a99ccaafa388e8128
unknown <kurcheti348@gmail.com>
2022-09-15 22:38:20 +0530

    Initial Commit

---
+++ A.java
@@ -0,0 +1,14 @@
+class A{
+   public int a = 1;
+   private int b = 2;
+   protected int c =3;
+         int d = 4;
+public static void main(String[] args){
+ A  objectA  = new A();
+
+System.out.println(objectA.a);
+System.out.println(objectA.b);
+System.out.println(objectA.c);
+System.out.println(objectA.d);
+}
+}
